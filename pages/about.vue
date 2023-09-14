0f72d60b2f0551f82f1ea97c514f5f238bb6e332
jeanquark <jm.kleger@gmail.com>
2023-09-14 11:20:16 +0200

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+        <p>This page will be displayed at the /about route.</p>
+        <NuxtLink to="/">&larr; Home</NuxtLink>
+    </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Welcome to the homepage</h1>
+        <AppAlert> This is an auto-imported component </AppAlert>
+        <br /><br />
+        <NuxtLink to="/about">About &rarr;</NuxtLink>
+    </div>
+</template>
